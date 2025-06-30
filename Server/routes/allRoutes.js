920d25558ca6555c2fa75c2d852c05336876b435
jeebanBeura <lifemagnifying@gmail.com>
2025-06-30 12:37:40 +0530

    the server files delete

--- Server/routes/allRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminRoutes = require('./adminRoutes');
-const userRoutes = require('./userRoutes');
-
-router.use('/admins', adminRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
