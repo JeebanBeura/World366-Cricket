2cc96d04b063d4fd55de1e53a46803c023635a54
jeebanBeura <lifemagnifying@gmail.com>
2025-07-05 22:48:34 +0530

    Update user model

--- Server/models/user.js
+++ Server/models/user.js
@@ -3,8 +3,27 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
-    }
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    bookmarks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'NewsArticle'
+    }],
+    preferences: [String], // e.g., ["sports", "tech"]
 });
 
 
